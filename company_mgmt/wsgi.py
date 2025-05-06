54b3be710cbf1e32008c58bb1722978c8c77ee69
Vaibh022001 <vaibhavpawar8408995805@gmail.com>
2025-05-06 18:47:45 +0530

    Add files via upload

--- company_mgmt/wsgi.py
+++ company_mgmt/wsgi.py
@@ -1,9 +1,3 @@
-# import os
-# from django.core.wsgi import get_wsgi_application
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'company_mgmt.settings')
-# application = get_wsgi_application()
-
 
 import os
 from django.core.wsgi import get_wsgi_application
